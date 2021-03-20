668f5f64d76ab7b4779dac3b679472c9df606b75
JoTiglao <Jotiglao1111@gmail.com>
2021-03-20 14:20:55 +0800

    Lab Joan

---
+++ Joan.java
@@ -0,0 +1,22 @@
+
+  @WebMethod
+  public String Example4(int answer){
+      String back = "Success";
+      String back1 = "Failed";
+      if(answer==6){return back;}
+      else return back;
+  }
+  @WebMethod
+  public String Example5(int answer){
+      String back = "Success";
+      String back1 = "Failed";
+      if(answer>7){return back;}
+      else return back1;
+  }
+ @WebMethod
+  public String Example5(int answer){
+      String back = "Success";
+      String back1 = "Failed";
+      if(answer<8){return back;}
+      else return back1;
+  }
